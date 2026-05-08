f1ce1448b6e3bb2e866dac2ab1128312f0fbb995
Andres Cabrera <andres.cabrera.calupina@udla.edu.ec>
2026-05-08 14:53:42 -0500

    se ingreso lo puesto por el profesor

--- trabajo1.c
+++ trabajo1.c
@@ -3,4 +3,19 @@
 #include <restar.h>
 
 
+int main(int argc, char const *argv[])
+{
+    int a,b;
+
+    printf("Ingrese el 1er número: ");
+    scanf("%d", &a);
+
+    printf("Ingrese el 2do número: ");
+    scanf("%d", &b);
+
+    printf("La suma es: %d\n", sumar(a,b));
+    printf("La resta es: %d", restar(a,b));
+
+    return 0;
+}
 
