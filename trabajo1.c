437f640ae7ae794b8e5a2ba559fdaa824a73ede3
Andres Cabrera <andres.cabrera.calupina@udla.edu.ec>
2026-05-08 15:11:23 -0500

    codigo final

--- trabajo1.c
+++ trabajo1.c
@@ -4,19 +4,42 @@
 #include "dividir.h"
 #include "multiplicar.h"
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    int a,b;
+    int a,b,c;
 
-    printf("Ingrese el 1er número: ");
+    printf("ingrese el 1er numero: ");
     scanf("%d", &a);
 
-    printf("Ingrese el 2do número: ");
+    printf("ingrese el 2do numero: ");
     scanf("%d", &b);
 
-    printf("La suma es: %d\n", sumar(a,b));
-    printf("La resta es: %d", restar(a,b));
+    printf("que operacion quiere realizar?\n");
+    printf("1.suma 2.resta 3.multiplicacion 4.division\n");
+
+    scanf("%d", &c);
+
+    switch (c)
+    {
+    case 1:
+        printf("La suma es: %d\n", sumar(a,b));
+        break;
+
+    case 2:
+        printf("La diferencia es: %d", restar(a,b));
+        break;
+
+    case 3:
+        printf("La multiplicacion es: %d", multiplicar(a,b));
+        break;
+
+    case 4:
+        printf("El cociente es: %d", dividir(a,b));
+        break;
+
+    default:
+        break;
+    }
 
     return 0;
 }
-
